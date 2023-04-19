e1096b86633f02cc9113ce477ebde89c1f4d6b1d
Carlos Pareja <carlospareja@iesvaldehierro.es>
2023-04-19 22:11:14 +0200

    Ejemplos de funciones

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("¡Hola mundo!");
 Console.ReadLine();
+
+    var nombre = Console.ReadLine();
+    EscribirMensaje("Carlos");
+    EscribirMensaje(nombre);
+    EscribirMensaje(nombre);
+    EscribirMensaje(nombre);
+    EscribirMensaje(nombre);
+    EscribirMensaje(nombre);
+    EscribirMensaje(nombre);
+
+
+  static string EscribirMensaje(string nombre)
+  {
+    Console.WriteLine("Adios " + nombre);
+    return "Hola" + nombre;
+  }
--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 console.log('¡Hola mundo!');
 alert('¡Hola mundo!');
+
+
+function mensaje (nombre) {
+    console.log('Hola ' + nombre);
+    return 'Hola ' + nombre;
+}
+
+var nombreUsuario = prompt("Dime tu nombre");
+mensaje(nombreUsuario);
+mensaje(nombreUsuario);
+mensaje(nombreUsuario);
+mensaje(nombreUsuario);
+mensaje(nombreUsuario);
+mensaje(nombreUsuario);
+mensaje(nombreUsuario);
